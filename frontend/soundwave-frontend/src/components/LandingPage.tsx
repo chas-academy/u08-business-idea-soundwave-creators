a64a4540cb8c59ea58f8c783e33afd9cbf20fc0f
SimretN <134206815+SimretN@users.noreply.github.com>
2024-05-10 18:47:27 +0200

    landing page initiated

---
+++ frontend/soundwave-frontend/src/components/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <>
+      <div className="">Album</div>
+      <div className="">Popular Music</div>
+      <div className="">Popular artist</div>
+    </>
+  );
+}
+
+export default LandingPage;
