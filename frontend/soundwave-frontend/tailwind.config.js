2e652d36e566faac1dbb4ade367e945851a1d645
DanielSedell02 <Danne.sedell@gmail.com>
2024-05-14 17:17:45 +0200

    new colors

--- frontend/soundwave-frontend/tailwind.config.js
+++ frontend/soundwave-frontend/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        secondary: "0 0 0.5rem rgba(246, 132, 2, 0.5)", // Using the secondary color as a box shadow
+      },
+      colors: {
+        primary: "#0f172a",
+        secondary: "#f68402",
+        hover: "#f68402",
+        text: "white",
+      },
+    },
   },
   plugins: [],
 };
