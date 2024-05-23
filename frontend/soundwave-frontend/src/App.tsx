7834c452a347d59ef7b58dacdb59f3d96923927b
DanielSedell02 <Danne.sedell@gmail.com>
2024-05-23 12:55:55 +0200

    Solved package.json error

--- frontend/soundwave-frontend/src/App.tsx
+++ frontend/soundwave-frontend/src/App.tsx
@@ -1,47 +1,43 @@
 // import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Aboutus from "./components/Aboutus/aboutus"
-import Contactus from './components/Contactus/contactus';
+import Aboutus from "./components/Aboutus/aboutus";
+import Contactus from "./components/Contactus/contactus";
 
-import HomePage from './components/HomePage/HomePage';
+import HomePage from "./components/HomePage/HomePage";
 
-
-
-import LoginPage from './components/login/login';
-import AdminDashboard from './components/dashboard/admindashboard';
-import UserDashboard from './components/dashboard/userdashboard';
-import Subscribe from './components/subscribe/subscribe';
-import Navbar from './components/Navbar/navbar';
-import Footer from './components/Footer/footer';
-
-
+// import LoginPage from './components/login/login';
+import AdminDashboard from "./components/dashboard/admindashboard";
+import UserDashboard from "./components/dashboard/userdashboard";
+import Subscribe from "./components/subscribe/subscribe";
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <>
-  
-    <Router>
-    <Navbar/>
-      <Routes>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
 
-      <Route path="/" element={<HomePage />} />
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route
+            path="/userdashboard"
+            element={
+              <UserDashboard fullName={""} emailAddress={""} createdAt={""} />
+            }
+          />
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          <Route path="/subscribe" element={<Subscribe />} />
 
-      <Route path="/admindashboard" element={<AdminDashboard />} />
-      <Route path="/userdashboard" element={<UserDashboard fullName={''} emailAddress={''} createdAt={''} />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/subscribe" element={<Subscribe />} />
-     
-
-        <Route path="/aboutus" element={<Aboutus />} />
-        <Route path="/contactus" element={<Contactus />} />
-      
-      </Routes>
-     <Footer/>
-    </Router>
-    
-   </>
-  )
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/contactus" element={<Contactus />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
