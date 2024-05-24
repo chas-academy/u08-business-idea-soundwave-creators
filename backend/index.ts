b3446c8882fef4b28790aa883b7b5590aa6104c3
jacobbirke <birkejacob@gmail.com>
2024-05-24 16:31:48 +0200

    musicPlayer_update

--- backend/index.ts
+++ backend/index.ts
@@ -1,15 +1,33 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './src/db/db';
+import albumRoutes from './src/routes/albumRoutes'; // Import album routes
+import artistRoutes from './src/routes/artistRoutes'; // Import artist routes
+import { getAllAlbums } from './src/controllers/albumcontroller'; 
 
 dotenv.config();
+connectDB();
 
-const app: Express = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
+app.use(express.json());
+
+// Define a simple route for testing
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+    res.send("Express + TypeScript");
 });
 
+app.get("/test", getAllAlbums);
+
+// Use album routes
+app.use('/api/albums', albumRoutes);
+
+// Use artist routes
+app.use('/api/artists', artistRoutes);
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
--- frontend/soundwave-frontend/src/App.tsx
+++ frontend/soundwave-frontend/src/App.tsx
@@ -1,21 +1,127 @@
-import React from 'react';
-import MusicPlayer from './components/MusicPlayer/MusicPlayer';
-import './index.css';
-import Navbar from './components/Navbar/navbar';
+// import React from 'react'
+// import { BrowserRouter as Router, Routes, Route,  } from "react-router-dom";
+
+// import Aboutus from "./components/Aboutus/aboutus";
+// import Contactus from "./components/Contactus/contactus";
+
+// import HomePage from "./components/HomePage/HomePage";
+
+// // import LoginPage from './components/login/login';
+// import AdminDashboard from "./components/dashboard/admindashboard";
+// import UserDashboard from "./components/dashboard/userdashboard";
+// import Subscribe from "./components/subscribe/subscribe";
+// import Navbar from "./components/Navbar/navbar";
+// import Footer from "./components/Footer/footer";
+// import GenrePage from "components/Genre/GenrePage";
+
+// function App() {
+//   const App: React.FC = () => {
+//     const genres = [
+//       "pop",
+//       "rock",
+//       "hip-hop",
+//       "jazz",
+//       "electronic",
+//       "folk",
+//       "classical",
+//       "country",
+//     ];
+
+//   return (
+//     <>
+//       <Router>
+//         <Navbar />
+//         <Routes>
+//           <Route path="/" element={<HomePage />} />
+
+//           <Route path="/admindashboard" element={<AdminDashboard />} />
+//           <Route
+//             path="/userdashboard"
+//             element={
+//               <UserDashboard fullName={""} emailAddress={""} createdAt={""} />
+//             }
+//           />
+//           {/* <Route path="/login" element={<LoginPage />} /> */}
+//           <Route path="/subscribe" element={<Subscribe />} />
+
+//           <Route path="/aboutus" element={<Aboutus />} />
+//           <Route path="/contactus" element={<Contactus />} />
+//           <Route path="/genres/:genre" element={<GenrePage />} />
+//           genres={genres}
+
+//         </Routes>
+//         <Footer />
+//       </Router>
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Aboutus from "./components/Aboutus/aboutus";
+import Contactus from "./components/Contactus/contactus";
+import HomePage from "./components/HomePage/HomePage";
+// import LoginPage from './components/login/login';
+import AdminDashboard from "./components/dashboard/admindashboard";
+import UserDashboard from "./components/dashboard/userdashboard";
+import Subscribe from "./components/subscribe/subscribe";
+import Navbar from "./components/Navbar/navbar";
+import Footer from "./components/Footer/footer";
+import GenrePage from "./components/Genre/GenrePage";
+import MusicPlayer from "./components/MusicPlayer/MusicPlayer";
 
 const App: React.FC = () => {
-  // Your App component logic here
+  const genres = [
+    "pop",
+    "rock",
+    "hip-hop",
+    "jazz",
+    "electronic",
+    "folk",
+    "classical",
+    "country",
+  ];
+
   return (
-    <div className="App">
-      <Navbar/>
-      <MusicPlayer
-        song={{
-          title: 'Song Title',
-          artist: 'Artist Name',
-          albumArt: 'path_to_album_art.jpg'
-        }}
-      />
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<HomePage />} />
+          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route
+            path="/userdashboard" */}
+            {/* element={
+              <UserDashboard fullName={""} emailAddress={""} createdAt={""} />
+            }
+          /> */}
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          {/* <Route path="/subscribe" element={<Subscribe />} />
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/contactus" element={<Contactus />} />
+          <Route
+            path="/genres/:genre"
+            element={<GenrePage genres={genres} />}
+          /> */}
+          <Route
+            path="/musicplayer"
+            element={
+              <MusicPlayer
+                song={{
+                  title: "Song Title",
+                  artist: "Artist Name",
+                  albumArt: "path_to_album_art.jpg",
+                }}
+              />
+            }
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   );
 };
 
