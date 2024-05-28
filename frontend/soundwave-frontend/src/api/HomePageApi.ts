9da175f24bb550927b30ec3b947b5ceab62aaa14
SimretN <134206815+SimretN@users.noreply.github.com>
2024-05-28 15:45:23 +0200

    HomePageApi created

---
+++ frontend/soundwave-frontend/src/api/HomePageApi.ts
@@ -0,0 +1,40 @@
+// services/api.ts
+import axios from 'axios';
+
+export interface Album {
+  _id: string;
+  name: string;
+  imageURL: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+// Function to fetch popular albums
+export const fetchPopularAlbums = async (): Promise<Album[]> => {
+  try {
+    const response = await axios.get<Album[]>('http://localhost:3000/api/albums');
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching popular albums:", error);
+    throw new Error("Failed to fetch popular albums. Please try again.");
+  }
+};
+
+export interface Artist {
+    _id: string;
+    name: string;
+    imageURL: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+  
+  // Function to fetch popular artists
+  export const fetchPopularArtists = async (): Promise<Artist[]> => {
+    try {
+      const response = await axios.get<Artist[]>('http://localhost:3000/api/albums');
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching popular artists:", error);
+      throw new Error("Failed to fetch popular artists. Please try again.");
+    }
+  };
